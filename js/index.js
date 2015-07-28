35afae70efad2474782ae9f6fc07de4b837832fb
Ted Wu <tedwu2016@u.northwestern.edu>
2015-07-28 00:56:58 -0700

    Add group object to database

--- js/index.js
+++ js/index.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
+    db = [];
+
+
+    var groupObject = {
+        name: " ",
+        tabs: []
+    };
+
+    $( "#addButton" ).click(function() {
+        value = $("#groupName").val();
+        groupObject.name = value;
+        console.log(groupObject);
+        db.push(groupObject);
+        console.log("db is", db);
+    });
+
+
+
     chrome.tabs.query({}, function(tabs) {
         $.each(tabs, function(idx, val) {
             $("#unsorted").append(val.url);
