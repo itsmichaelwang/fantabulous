3cfec0561164d12fbbd9910e837b785c46634356
Michael Wang <mwang@splunk.com>
2015-07-28 00:56:58 -0700

    Create a render function to display tab groups

--- js/index.js
+++ js/index.js
@@ -1,7 +1,49 @@
 $(document).ready(function() {
+    var db = [
+        {
+            name: "Unsorted Tabs",
+            tabs: []
+        },
+        {
+            name: "WildHacks",
+            tabs: []
+        },
+        {
+            name: "Job Search",
+            tabs: []
+        }
+    ]
+
     chrome.tabs.query({}, function(tabs) {
         $.each(tabs, function(idx, val) {
-            $("#unsorted").append(val.url);
+            db[0].tabs.push(val);
         });
+
+        render(db);
     });
 });
+
+var render = function(db) {
+    $('.row-fluid').empty();
+    // create a column for each tab group
+    $.each(db, function(idx, val) {
+        var col =
+            $('<div/>', {
+                class: 'col-lg-3',
+            }).append(
+                $('<h1/>', {
+                    class: 'panel-heading',
+                    text: val.name
+                })
+            );
+
+        // populate tab groups with tabs inside
+        $.each(val.tabs, function(idx, val) {
+            $('<p/>', {
+                text: val.title
+            }).appendTo(col);
+        });
+
+        col.appendTo('.row-fluid');
+    });
+}
