17d2a95d1a2b6312aff15d3d0d8ac7b20f34b938
Michael Wang <mwang@splunk.com>
2015-07-28 22:58:56 -0700

    Returned to yesterday's state with backbone

--- js/index.js
+++ js/index.js
@@ -6,6 +6,10 @@
         }
     });
 
+    var TabView = Backbone.View.extend({
+
+    });
+
     var MainView = Backbone.View.extend({
         initialize: function() {
             var workSpace = this.workSpace = new WorkSpace();
@@ -21,22 +25,22 @@
                     temp.push(t);
                 });
                 workSpace.set('ungrouped', temp);
-                workSpace.set('WildHacks', temp);
+                workSpace.set('WildHacks', []);
 
                 isLoaded.set('loaded', true);
             });
         },
         render: function() {
-            _.each(this.workSpace.attributes, function(item, index, items) {
+            _.each(this.workSpace.attributes, function(item, groupName, items) {
                 $('#groups').append('\
-                    <div id="' + index + '" class="col-lg-3">\
-                        <h1>' + index + '</h1>\
+                    <div id="' + groupName + '" class="col-lg-3">\
+                        <h1>' + groupName + '</h1>\
                     </div>');
 
                 _.each(item, function(item, index, items) {
                     console.log(item);
-                    
-                })
+                    $('#' + groupName).append('<div>' + item.get('url') + '</div>');
+                });
             }, this);
         }
     });
