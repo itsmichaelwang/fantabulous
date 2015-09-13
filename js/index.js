033c36d520787ca2dc4c0a344b64cefa3092a7e2
Michael Wang <mwang@splunk.com>
2015-09-13 01:32:31 -0700

    Refactor with simpler models, change add button

--- js/index.js
+++ js/index.js
@@ -1,90 +1,69 @@
 $(document).ready(function() {
-    var WorkSpace = Backbone.Model.extend({
-        defaults: {
-            'ungrouped': []
-        }
-    });
-
-    var TabView = Backbone.View.extend();
-
+    
     var ButtonView = Backbone.View.extend({
-
         events : {
-            'click #addButton': 'addGroup'
+            'click .add-button': 'addGroup'
         },
-
         addGroup: function () {
-            var userInput = $('#groupName').val();
-            if("userInput" in this.model.attributes){
-            }else{
-                this.model.set(userInput, []);
-            }
+            console.log("clicked");
+            // var userInput = $('#groupName').val();
+            // if("userInput" in this.model.attributes){
+            // } else {
+            //     this.model.set(userInput, []);
+            // }
         },
-
         render: function() {
             this.$el.html('');
-            this.$el.append('\
-                <div class="row-fluid">\
-                  <div class="col-lg-6">\
-                    <div class="input-group">\
-                      <span class="input-group-btn">\
-                        <button id="addButton" class="btn btn-default" type="button">Add Group</button>\
-                      </span>\
-                      <input id="groupName" type="text" class="form-control" placeholder="Search for...">\
-                    </div>\
-                  </div>\
-                </div>');
+            this.$el.append('<div class="add-button"><span class="glyphicon glyphicon-plus"></span></div>');
             return this;
         }
-
     });
 
     var MainView = Backbone.View.extend({
-
         initialize: function() {
-            var workSpace = this.workSpace = new WorkSpace();
-            var isLoaded = this.isLoaded = new Backbone.Model({
-                loaded: false
-            });
-            this.listenTo(this.isLoaded, 'change:loaded', this.render);
-            this.listenTo(this.workSpace, 'change', this.render);
-
-            chrome.tabs.query({}, function(tabs) {
-                var temp = [];
-                _.each(tabs, function(tab) {
-                    var t = new Backbone.Model(tab);
-                    temp.push(t);
-                });
-                workSpace.set('ungrouped', temp);
-                workSpace.set('WildHacks', []);
-
-                isLoaded.set('loaded', true);
-            });
+            this.workspace = {};
+            this.loaded = $.Deferred();
+            
+            chrome.tabs.query({}, (function(tabs) {
+                _.each(tabs, function(tab, idx) {
+                    if (tab.pinned) {
+                        this.workspace.pinned = this.workspace.pinned || {};
+                        this.workspace.pinned.name = this.workspace.pinned.name || "Pinned Tabs";
+                        this.workspace.pinned[tab.id] = tab;
+                    } else {
+                        this.workspace[tab.windowId] = this.workspace[tab.windowId] || {};
+                        this.workspace[tab.windowId].name = this.workspace[tab.windowId].name || "Window " + tab.windowId;
+                        this.workspace[tab.windowId][tab.id] = tab;
+                    }
+                    
+                }, this);
+                
+                this.loaded.resolveWith(this);
+            }).bind(this));
         },
-
         render: function() {
             this.$el.html('');
-            _.each(this.workSpace.attributes, function(item, groupName, items) {
-                this.$el.append('\
-                    <div id="' + groupName + '" class="col-lg-3">\
-                        <h1>' + groupName + '</h1>\
-                    </div>');
-
-                _.each(item, function(item, index, items) {
-                    this.$('#' + groupName).append('<div>' + item.get('url') + '</div>');
+            this.loaded.done(function() {
+                _.each(this.workspace, function(tabGroup, tabGroupId) {
+                    this.$el.append('<div id="column' + tabGroupId + '" class="col-lg-3"></div>')
+                    this.$('#column' + tabGroupId).append('<div id=' + tabGroupId + ' class="panel panel-default"></div>');
+                    this.$('#' + tabGroupId).append('<div class="panel-heading"><h3>' + tabGroup.name + '</h3></div>');
+                    this.$('#' + tabGroupId).append('<ul class="list-group"></ul>');
+                    
+                    _.each(tabGroup, function(tab, tabId) {
+                        this.$('#' + tabGroupId + " ul").append('<li class="list-group-item">' + tab.title + '</li>');
+                    }, this);
                 }, this);
-            }, this);
-
-            this.buttonView = new ButtonView({
-                model: this.workSpace
+                
+                this.buttonView = new ButtonView();
+                this.$el.append(this.buttonView.render().el);
+                
             });
-            this.$el.append(this.buttonView.render().el);
-
             return this;
+            
         }
     });
 
     var mainView = new MainView();
-    $('body').append(mainView.render().el);
-
+    $('#workspace').append(mainView.render().el);
 });
