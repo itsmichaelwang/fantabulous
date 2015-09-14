4a1699f42a62485b87d2113475e8d991cb14c5f6
Michael Wang <mwang@splunk.com>
2015-09-13 20:48:06 -0700

    Add drag and drop functionality with jQueryUI sortables

--- js/index.js
+++ js/index.js
@@ -12,6 +12,36 @@ $(document).ready(function() {
         initialize: function() {
             this.workspace = {};
             this.children = this.children || {};
+            
+            this.refreshTabs();
+            this.children.buttonView = new ButtonView();
+        },
+        render: function() {
+            this.$el.html('');
+            this.loaded.done(function() {
+                _.each(this.workspace, function(tabGroup, tabGroupId) {
+                    this.$el.append('<div id="column' + tabGroupId + '" class="col-lg-3"></div>')
+                    this.$('#column' + tabGroupId).append('<div id=' + tabGroupId + ' class="panel panel-default"></div>');
+                    this.$('#' + tabGroupId).append('<div class="panel-heading"><h3>' + tabGroup.name + '</h3></div>');
+                    this.$('#' + tabGroupId).append('<ul class="list-group"></ul>');
+                    this.$('#' + tabGroupId + ' .list-group').sortable({
+                        connectWith: ".list-group"
+                    });
+                    
+                    _.each(tabGroup, function(tab, tabId) {
+                        if (tabId != "name") {
+                            this.$('#' + tabGroupId + " ul").append('<li id="' + tabId + '" class="list-group-item">' + tab.title + '</li>');
+                            // this.$('#' + tabId).draggable();
+                        }
+                    }, this);
+                }, this);
+            });
+            
+            this.$el.append(this.children.buttonView.render().el);
+            return this;
+            
+        },
+        refreshTabs: function() {
             this.loaded = $.Deferred();
             
             chrome.tabs.query({}, (function(tabs) {
@@ -31,33 +61,15 @@ $(document).ready(function() {
                 this.loaded.resolveWith(this);
             }).bind(this));
             
-            this.children.buttonView = new ButtonView();
-            
-        },
-        render: function() {
-            this.$el.html('');
-            this.loaded.done(function() {
-                _.each(this.workspace, function(tabGroup, tabGroupId) {
-                    this.$el.append('<div id="column' + tabGroupId + '" class="col-lg-3"></div>')
-                    this.$('#column' + tabGroupId).append('<div id=' + tabGroupId + ' class="panel panel-default"></div>');
-                    this.$('#' + tabGroupId).append('<div class="panel-heading"><h3>' + tabGroup.name + '</h3></div>');
-                    this.$('#' + tabGroupId).append('<ul class="list-group"></ul>');
-                    
-                    _.each(tabGroup, function(tab, tabId) {
-                        if (tabId != "name") {
-                            this.$('#' + tabGroupId + " ul").append('<li class="list-group-item">' + tab.title + '</li>');
-                        }
-                    }, this);
-                }, this);
-            });
-            
-            this.$el.append(this.children.buttonView.render().el);
-            return this;
-            
         },
         events: {
             'click .add-button': function() {
-                console.log(this.workspace);
+                chrome.windows.create({
+                    state: 'minimized'
+                }, (function(window) {
+                    this.refreshTabs();
+                    this.render();
+                }).bind(this));
             }
         }
     });
