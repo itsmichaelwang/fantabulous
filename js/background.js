a6b0154de4b16dee01169eaafaf664507ef11dfe
Michael Wang <mwang@splunk.com>
2015-09-13 23:59:48 -0700

    Change manifest and other minor parameters until they work well

--- js/background.js
+++ js/background.js
@@ -1,9 +1,10 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.windows.update(tab.windowId, {
-        state: 'minimized'
+        // state: 'minimized'   // we'll figure out what to do later
     }, function(window) {
         chrome.windows.create({
-            url: "index.html"
+            url: "index.html",
+            type: "panel"
         });
     });
 });
